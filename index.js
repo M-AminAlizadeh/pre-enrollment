c66a06862de5a174f5f9c4f55855a174369bbbaf
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-03-08 17:02:46 +0330

    clean css and js codes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const hamburger = document.querySelector(".hamburger");
 const menu_container = document.querySelector(".menu-container");
 window.addEventListener("click",function(e){
     if(e.target == hamburger){
-        console.log("true")
         menu_container.classList.add("menu-toggler");
     }else{
         menu_container.classList.remove("menu-toggler");
