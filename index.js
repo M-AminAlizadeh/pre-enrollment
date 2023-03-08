2c6cefa3795122ebc73745ae27bac1b565e232f7
M-AminAlizadeh <mohammadaminalaizadeh1378@gmail.com>
2023-03-08 16:52:41 +0330

    Write functionality for Hamburger toggling in index.js

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// Menu Footer
 const year = new Date().getFullYear();
 const menu_footer_copyright = document.querySelector(".menu-footer-copyright-container");
 menu_footer_copyright.innerHTML = "&copy; " + year + " Google LLC";
+
+// Hamburger Toggle
+const hamburger = document.querySelector(".hamburger");
+const menu_container = document.querySelector(".menu-container");
+window.addEventListener("click",function(e){
+    if(e.target == hamburger){
+        console.log("true")
+        menu_container.classList.add("menu-toggler");
+    }else{
+        menu_container.classList.remove("menu-toggler");
+    }
+});
